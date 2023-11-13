909eb15a7929c279700875bd73b2d0a0ef529ee7
Aliaksei Karseka <al.karseka@andersenlab.com>
2023-11-13 01:30:07 +0100

    FB-014-ubdate_git_hub_pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from '../src/redux/store';
@@ -8,11 +7,7 @@ import { UserList } from "./pages/UserList/UserList";
 const App = () => {
   return (
       <Provider store={store}>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<UserList />} />
-              </Routes>
-          </BrowserRouter>
+          <UserList />
       </Provider>
   );
 }
